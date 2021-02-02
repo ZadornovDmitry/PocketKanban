4e025ef7ddfeee174a1ae2cd1844da2c70569868
Zadornov Dmitry <dimfloyd@gmail.com>
2021-02-02 19:02:38 +0300

    some changed

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+
     if (engine.rootObjects().isEmpty())
         return -1;
 
